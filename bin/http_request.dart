c7a39bc50615de96b70880c5afb327d29aa6a345
anproa31 <antoeta31@gmail.com>
2023-08-07 13:00:46 +0700

    handling error

--- bin/http_request.dart
+++ bin/http_request.dart
@@ -5,17 +5,27 @@ import 'package:http/http.dart' as http;
 import '../model/todo.dart';
 
 Future<void> main() async {
-  final url = 'https://jsonplaceholder.typicode.com/todos/1';
-  final parseUrl = Uri.parse(url);
-  final response = await http.get(parseUrl);
-  final statusCode = response.statusCode;
-  if (statusCode != 200) {
-    throw HttpException('Request failed with status code $statusCode');
+  try {
+    final url = 'https://jsonplaceholder.typicode.com/todos/1';
+    final parseUrl = Uri.parse(url);
+    final response = await http.get(parseUrl);
+    final statusCode = response.statusCode;
+    if (statusCode != 200) {
+      throw HttpException('Request failed with status code $statusCode');
+    }
+
+    final jsonString = response.body;
+    dynamic jsonMap = json.decode(jsonString);
+
+    final todo = Todo.fromJson(jsonMap);
+    print(todo);
+  } on SocketException catch (e) {
+    print('SocketException: $e');
+  } on HttpException catch (e) {
+    print('HttpException: $e');
+  } on FormatException catch (e) {
+    print('FormatException: $e');
+  } catch (e) {
+    print('Exception: $e');
   }
-
-  final jsonString = response.body;
-  dynamic jsonMap = json.decode(jsonString);
-
-  final todo = Todo.fromJson(jsonMap);
-  print(todo);
 }
